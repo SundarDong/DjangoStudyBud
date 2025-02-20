a117125ac2dfbf0aef1eebe682bc05d947843d6b
SundarDong <brosisstudio123@gmail.com>
2025-02-20 12:17:36 +0545

    feat: Learning list rendering in the home.html template

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DjangoStudyBud
+
+1. I started to write the README.md file from studying the rooms list for rendering in the home.html .
--- base/views.py
+++ base/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 # Create your views here.
+
+rooms =[
+    {'id':1, 'name':'Lets learn python!'},
+    {'id':2, 'name':'Design with me'},
+    {'id':3, 'name':'Frontend Developers'},
+]
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html',{'rooms':rooms})
 
 def room(request):
     return render(request, 'room.html')
